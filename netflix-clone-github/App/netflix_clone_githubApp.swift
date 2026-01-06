cf8e0b71bb42990dd1425c4e5ba6184411f83ba3
Pardip Bhatti <pardipbhatti8791@gmail.com>
2026-01-06 00:12:45 -0500

    initial commit

---
+++ netflix-clone-github/App/netflix_clone_githubApp.swift
@@ -0,0 +1,19 @@
+//
+//  netflix_clone_githubApp.swift
+//  netflix-clone-github
+//
+//  Created by Pardip Bhatti on 01/01/26.
+//
+
+import SwiftUI
+import ToastUI
+
+@main
+struct netflix_clone_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .setupToastUI()
+        }
+    }
+}
